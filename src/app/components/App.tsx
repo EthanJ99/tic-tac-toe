aa0be29d4559848bf0c41c6716cb776a496eec65
EthanJ99 <9293417+EthanJ99@users.noreply.github.com>
2025-10-03 14:13:54 +0100

    Refactor board rendering; general cleanup

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -8,7 +8,7 @@ interface SquareProps {
   onSquareClick: MouseEventHandler
 }
 
-function Square({value, onSquareClick}: SquareProps) {
+function Square({ value, onSquareClick }: SquareProps) {
   return (
     <button
       className="square"
@@ -23,61 +23,46 @@ interface BoardProps {
   xIsNext: boolean,
   squares: SquareValue[],
   onPlay: (nextSquares: SquareValue[]) => void;
-  
+
 }
 
 function Board({ xIsNext, squares, onPlay }: BoardProps) {
   const winner = calculateWinner(squares);
   let status: string;
 
-  if(winner){
+  if (winner) {
     status = "Winner: " + winner;
-  } else{
+  } else {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
   // Update value of individual square when user clicks it
   function handleClick(clickedSquare: number) {
-    if(squares[clickedSquare] || calculateWinner(squares)){
+    if (squares[clickedSquare] || calculateWinner(squares)) {
       // Don't update if square is already filled, or if someone has won
       return;
     }
     const nextSquares: SquareValue[] = squares.slice();
-    nextSquares[clickedSquare] = xIsNext ?  "X" : "O";
+    nextSquares[clickedSquare] = xIsNext ? "X" : "O";
 
     onPlay(nextSquares);
   }
 
-
   return (
     <>
       <div className="status">{status}</div>
+      {[...Array(3)].map((_, row) => (
+        <div className="board-row" key={row}>
+          {[...Array(3)].map((_, col) => {
+            const squareIndex = row * 3 + col;
+            return (
+              <Square key={squareIndex} value={squares[squareIndex]} onSquareClick={() => handleClick(squareIndex)} />
+            )
+          })}
+        </div>
+      )
+      )}
 
-      
-      {/*
-      for int x = 0; x < 3; x++
-        div open
-        for int y = 0; y < 3; y++
-          square val = squares(x * 3 + y) onSquareClick = handlecli(x * 3 + y)
-
-      */}
-
-
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-      </div>
     </>
   );
 }
@@ -88,22 +73,22 @@ export default function Game() {
   const xIsNext = currentMove % 2 == 0;
   const currentSquares = history[currentMove];
 
-  function handlePlay(nextSquares: SquareValue[]){
+  function handlePlay(nextSquares: SquareValue[]) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
   }
 
-  function jumpTo(nextMove: number){
+  function jumpTo(nextMove: number) {
     setCurrentMove(nextMove);
   }
 
   const moves = history.map((squares, move) => {
     let description;
 
-    if(move > 0){
+    if (move > 0) {
       description = 'Go to move #' + move;
-    } else{
+    } else {
       description = 'Go to game start';
     }
     return (move === currentMove) ? (
@@ -118,7 +103,7 @@ export default function Game() {
   return (
     <div className="game">
       <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
         <ol>{moves}</ol>
@@ -142,7 +127,7 @@ function calculateWinner(squares: SquareValue[]) {
     [0, 4, 8],
     [2, 4, 6]
   ];
-  
+
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
